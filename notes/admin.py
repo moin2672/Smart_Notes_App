e87a4353a2795bc4b8bf1646018e282e77f936a4
moin2672 <moin2672@gmail.com>
2022-07-19 18:15:15 +0530

    11a. using admin for Notes data display

--- notes/admin.py
+++ notes/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from . import models
 
 class NotesAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title',)
 
 admin.site.register(models.Notes, NotesAdmin)
