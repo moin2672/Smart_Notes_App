17f5eec896f35b216a6dd5a03ce7ae2afa638069
moin2672 <moin2672@gmail.com>
2022-07-19 18:13:43 +0530

    11. using admin for Notes data creation and manipulation

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+class NotesAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Notes, NotesAdmin)
