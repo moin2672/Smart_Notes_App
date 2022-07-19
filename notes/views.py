33828e535e7a9677943fffa08cf1e63857c603a6
moin2672 <moin2672@gmail.com>
2022-07-19 22:56:46 +0530

    18. auto form creation for POST request

--- notes/views.py
+++ notes/views.py
@@ -2,7 +2,7 @@ from typing import List
 from django.shortcuts import render
 from django.http import Http404
 
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 from .models import Notes
 
@@ -16,3 +16,8 @@ class NotesDetailView(DetailView):
     model = Notes
     context_object_name = "note"
     #template_name = "notes/note_detail.html"
+
+class NotesCreateView(CreateView):
+    model= Notes
+    fields = ['title', 'text']
+    success_url = '/smart/notes'
