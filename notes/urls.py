911537e342102fd9a9e0ea5b0aa155bb7e9e3528
moin2672 <moin2672@gmail.com>
2022-07-19 22:40:45 +0530

    17. adding bootstrap styles

--- notes/urls.py
+++ notes/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes', views.NoteListView.as_view()),
-    path('notes/<int:pk>', views.NotesDetailView.as_view())
+    path('notes', views.NoteListView.as_view(), name="notes.list"),
+    path('notes/<int:pk>', views.NotesDetailView.as_view(), name="notes.detail")
 ]
