f2981d7bde2519969fc74473aaf2c892ed276230
moin2672 <moin2672@gmail.com>
2022-07-19 22:03:32 +0530

    15a. django class based views - a bit more

--- notes/urls.py
+++ notes/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes', views.list),
-    path('notes/<int:pk>', views.detail)
+    path('notes', views.NoteListView.as_view()),
+    path('notes/<int:pk>', views.NotesDetailView.as_view())
 ]
