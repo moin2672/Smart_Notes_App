68c36b1e841b1afddf209494489371d1c9c2387c
moin2672 <moin2672@gmail.com>
2022-07-20 00:42:32 +0530

    26. adding Sigup page

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,8 @@ from django.views import View
 from . import views
 
 urlpatterns = [
-    path('home', views.HomeView.as_view(), name='home'),
+    path('', views.HomeView.as_view(), name='home'),
     path('login', views.LoginInterfaceView.as_view(), name='login'),
     path('logout', views.LogoutInterfaceView.as_view(), name='logout'),
+    path('signup', views.SignUpView.as_view(), name='signup'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from datetime import datetime
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
 
 class HomeView(TemplateView):
     template_name = 'home/welcome.html'
@@ -17,3 +19,13 @@ class LoginInterfaceView(LoginView):
 
 class LogoutInterfaceView(LogoutView):
     template_name = 'home/logout.html'
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'home/register.html'
+    success_url = '/smart/notes'
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('notes.list')
+        return super().get(request, *args, **kwargs)
