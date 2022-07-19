a6b8f872140693ca5f5c0494d8da4bdada5d04a5
moin2672 <moin2672@gmail.com>
2022-07-19 21:56:30 +0530

    15. django class based views

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,6 @@ from django.views import View
 from . import views
 
 urlpatterns = [
-    path('home', views.home),
-    path('authorized', views.authorized),
+    path('home', views.HomeView.as_view()),
+    path('authorized', views.AuthorizedView.as_view()),
 ]
